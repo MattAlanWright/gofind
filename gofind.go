75d9069c83f24aef22030839be4b78c5eb5603b0
Matthew Wright <mwright@ourmagnet.net>
2024-02-04 13:24:34 -0500

    Tweak error handling

--- gofind.go
+++ gofind.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+const verbose = false
 
 func isDirectory(path string) bool {
 	fileInfo, err := os.Stat(path)
@@ -21,16 +24,23 @@ func main() {
 	// TODO: Use CLI package
 	var numArgs = len(os.Args)
 	if numArgs == 1 {
-		fmt.Println("Why no argament?")
+		fmt.Println("Must provide pattern argument")
 		return
 	}
 
 	pattern := os.Args[1]
 	var rootDirectory string
+
+	var err error
 	switch numArgs {
 	case 2:
 		// TODO: Error handling here
-		rootDirectory, _ = os.Getwd()
+		rootDirectory, err = os.Getwd()
+		if err != nil {
+			log.Fatal("Failed to get current directory")
+			return
+		}
+
 	case 3:
 		rootDirectory = os.Args[2]
 	}
@@ -44,8 +54,25 @@ func main() {
 	fmt.Printf("Looking for '%v' in '%v'\n", pattern, rootDirectory)
 
 	filepath.Walk(rootDirectory, func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
+		if err == fs.SkipDir {
+			if verbose {
+				fmt.Printf("Skipping directory %v\n", path)
+			}
 			return err
+		}
+
+		if err == fs.SkipAll {
+			if verbose {
+				fmt.Printf("Skipping ALL at path %v\n", path)
+			}
+			return err
+		}
+
+		if err != nil {
+			if verbose {
+				fmt.Printf("Error in WalkFunc - %v\n", err)
+			}
+			return nil
 		}
 
 		if !info.IsDir() && strings.Contains(path, pattern) {
